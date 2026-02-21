2b7234efbccdd24421bb1db0a3fe5d8214ccf3b1
Mario Castro <mariocastro73@gmail.com>
2026-02-21 19:52:57 +0100

    fixed version for new pymc requirements

--- lotka_ABCSMC.py
+++ lotka_ABCSMC.py
@@ -29,74 +29,75 @@ def competition_model(rng, a, b, size=None):
 
 
 # Step 5: Bayesian inference with PyMC
-with pm.Model() as model_lv:
-    # Priors
-    a = pm.HalfNormal("a", 1.0)
-    b = pm.HalfNormal("b", 1.0)
-    # Likelihood (ABC). Epsilon is the initial tolerance
-    sim = pm.Simulator("sim", competition_model, params=(a, b), epsilon=10, observed=observed)
-    # Inference
-    samples = pm.sample_smc(draws=500, chains=4) # Faster for testing
-    # Convert to ArviZ InferenceData
-    posterior = samples.posterior.stack(samples=("draw", "chain"))
-    # post = posterior.to_pandas()
+if __name__ == "__main__":
+    with pm.Model() as model_lv:
+        # Priors
+        a = pm.HalfNormal("a", 1.0)
+        b = pm.HalfNormal("b", 1.0)
+        # Likelihood (ABC). Epsilon is the initial tolerance
+        sim = pm.Simulator("sim", competition_model, params=(a, b), epsilon=10, observed=observed)
+        # Inference
+        samples = pm.sample_smc(draws=500, chains=4) # Faster for testing
+        # Convert to ArviZ InferenceData
+        posterior = samples.posterior.stack(samples=("draw", "chain"))
+        # post = posterior.to_pandas()
 
-az.summary(samples, hdi_prob=0.95)
+    az.summary(samples, hdi_prob=0.95)
 
-# Plotting
-## Plot posterior predictive
-_, ax = plt.subplots(figsize=(14, 6))
-ax.plot(t, observed_matrix[:, 0], "o", label="prey", c="C0", mec="k")
-ax.plot(t, observed_matrix[:, 1], "o", label="predator", c="C1", mec="k")
+    # Plotting
+    ## Plot posterior predictive
+    _, ax = plt.subplots(figsize=(14, 6))
+    ax.plot(t, observed_matrix[:, 0], "o", label="prey", c="C0", mec="k")
+    ax.plot(t, observed_matrix[:, 1], "o", label="predator", c="C1", mec="k")
 
-mean_a = posterior["a"].mean().item()
-mean_b = posterior["b"].mean().item()
-mean_sim = odeint(dX_dt, y0=X0, t=t, rtol=0.01, args=(mean_a, mean_b, c, d))
-ax.plot(t, mean_sim[:, 0], linewidth=3, label="mean prey", c="C0")
-ax.plot(t, mean_sim[:, 1], linewidth=3, label="mean predator", c="C1")
+    mean_a = posterior["a"].mean().item()
+    mean_b = posterior["b"].mean().item()
+    mean_sim = odeint(dX_dt, y0=X0, t=t, rtol=0.01, args=(mean_a, mean_b, c, d))
+    ax.plot(t, mean_sim[:, 0], linewidth=3, label="mean prey", c="C0")
+    ax.plot(t, mean_sim[:, 1], linewidth=3, label="mean predator", c="C1")
 
-for i in np.random.randint(0, posterior.samples.size, 75):
-    ai = posterior["a"].values[i]
-    bi = posterior["b"].values[i]
-    sim_i = odeint(dX_dt, y0=X0, t=t, rtol=0.01, args=(ai, bi, c, d))
-    ax.plot(t, sim_i[:, 0], alpha=0.1, c="C0")
-    ax.plot(t, sim_i[:, 1], alpha=0.1, c="C1")
+    for i in np.random.randint(0, posterior.samples.size, 75):
+        ai = posterior["a"].values[i]
+        bi = posterior["b"].values[i]
+        sim_i = odeint(dX_dt, y0=X0, t=t, rtol=0.01, args=(ai, bi, c, d))
+        ax.plot(t, sim_i[:, 0], alpha=0.1, c="C0")
+        ax.plot(t, sim_i[:, 1], alpha=0.1, c="C1")
 
-ax.set_xlabel("time")
-ax.set_ylabel("population")
-ax.legend()
-plt.show()
+    ax.set_xlabel("time")
+    ax.set_ylabel("population")
+    ax.legend()
+    plt.show()
 
-## Plot posterior distributions
-az.plot_posterior(samples, kind="hist", bins=30)
-plt.show()
+    ## Plot posterior distributions
+    az.plot_posterior(samples, kind="hist", bins=30)
+    plt.show()
 
-## Plot diagnostics
-az.plot_trace(samples)
-# az.plot_trace(samples, kind="rank_bars") # Alternative
-plt.suptitle(f"Trace Plot");
-plt.show()
+    ## Plot diagnostics
+    az.plot_trace(samples)
+    # az.plot_trace(samples, kind="rank_bars") # Alternative
+    plt.suptitle(f"Trace Plot");
+    plt.show()
 
-# with variables a and b in variable posterior, plot a heatmap
-plt.hist2d(posterior["a"].values, posterior["b"].values, bins=100, cmap="Blues")
-plt.colorbar(label="Counts")
-plt.xlabel("a")
-plt.ylabel("b")
-plt.title("2D Histogram of Posterior Samples for a and b")
-plt.show()
+    # with variables a and b in variable posterior, plot a heatmap
+    plt.hist2d(posterior["a"].values, posterior["b"].values, bins=100, cmap="Blues")
+    plt.colorbar(label="Counts")
+    plt.xlabel("a")
+    plt.ylabel("b")
+    plt.title("2D Histogram of Posterior Samples for a and b")
+    plt.show()
 
-# same but with kde
-# plt.figure(figsize=(8, 6))
-az.plot_kde(posterior["a"].values, posterior["b"].values, fill_last=True)
-# az.plot_kde(posterior["a"].values, posterior["b"].values, hdi_probs=[0.05, 0.5, 0.95]) # Alternative
-plt.xlabel("a")
-plt.ylabel("b")
-plt.title("KDE of Posterior Samples for a and b")
-plt.show()
+    # same but with kde
+    # plt.figure(figsize=(8, 6))
+    az.plot_kde(posterior["a"].values, posterior["b"].values, fill_last=True)
+    # az.plot_kde(posterior["a"].values, posterior["b"].values, hdi_probs=[0.05, 0.5, 0.95]) # Alternative
+    plt.xlabel("a")
+    plt.ylabel("b")
+    plt.title("KDE of Posterior Samples for a and b")
+    plt.show()
 
-# plt.figure(figsize=(8, 6))
-az.plot_autocorr(samples)
-plt.show()
+    # plt.figure(figsize=(8, 6))
+    az.plot_autocorr(samples)
+    plt.show()
 
 
 
